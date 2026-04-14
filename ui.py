23d8e9fd29c07fd8492c4c0137b2c66fbb4015bd
Johel Fernandez <johel.alonso@gmail.com>
2026-04-13 22:03:32 -0600

    Se agrega ui que es donde va el menu mas adelante se agregan mas funciones como la impresion

---
+++ ui.py
@@ -0,0 +1,20 @@
+#ui.py
+#Realizado por Johel Fernandez y Isaac Esquivel 4/13/2026 9:14PM
+
+import utils
+
+def menuPrincipal():
+    """
+    El propolsito de esta funcion es mostrar las opciones principales
+    del sistema DevSpaces.
+    No recibe parametros ni retorna nada
+    """
+
+    utils.limpiarPantalla()
+    print("   MENU DEVSPACES   ")
+    print("1. Consultar todos los Usuarios")
+    print("2. Consultar todos los Spaces")
+    print("3. Seguir un Space")
+    print("4. Consultar mis Spaces seguidos")
+    print("5. Gestionar seguidores de mis Spaces")
+    print("6. Salir")
