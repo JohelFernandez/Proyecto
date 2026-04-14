095a8dacd0dacc7ab4fb0d5c3a66cd5d39f4bc3c
Johel Fernandez <johel.alonso@gmail.com>
2026-04-13 23:58:02 -0600

    Se creo el controlador que va a tener toda la logica de la aplicacion y se modifico utils donde vamos a declarar todos los colores ASCI que vamos a utilizar

---
+++ controller.py
@@ -0,0 +1,8 @@
+#controller.py
+#Realizado por Johel Fernandez y Isaac Esquivel 4/13/2026 10:07PM
+
+import devspace
+import utils
+import ui
+
+
--- ui.py
+++ ui.py
@@ -1,6 +1,12 @@
 #ui.py
 #Realizado por Johel Fernandez y Isaac Esquivel 4/13/2026 9:14PM
+"""
+Modulo de la interfaz para el usuario
 
+Contiene todas las funciones que va a ver el usuario como menus y presentacion
+de datos para que el usuario interactue con la applicacion a traves de la 
+terminal
+"""
 import utils
 
 def menuPrincipal():
@@ -11,10 +17,13 @@ def menuPrincipal():
     """
 
     utils.limpiarPantalla()
-    print("   MENU DEVSPACES   ")
+    print("---------MENU DEVSPACES---------")
     print("1. Consultar todos los Usuarios")
     print("2. Consultar todos los Spaces")
     print("3. Seguir un Space")
     print("4. Consultar mis Spaces seguidos")
     print("5. Gestionar seguidores de mis Spaces")
     print("6. Salir")
+
+
+
