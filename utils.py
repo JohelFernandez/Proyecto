515f4256f1f218dc732de45163c747c459b5f1cd
Johel Fernandez <johel.alonso@gmail.com>
2026-04-13 21:11:09 -0600

    Se crea el utils que es para limpiar pantalla y pausar mas adelante se tiene que agregar una animacion para lineaas y un texto de comandos para resaltar comandos de python

---
+++ utils.py
@@ -0,0 +1,28 @@
+#utils.py
+#Realizado por Johel Fernandez y Isaac Esquivel 10:25AM 4/13/2026
+
+from os import system
+import time
+
+def limpiarPantalla():
+    """
+    El proposito de esta funcion es limpiar la consola para tener
+    un menu ordenado y estetico.
+    Parametros ninguno y no retorna nada.
+    """
+    system("cls")
+
+def pausarPantalla():
+    """
+    El proposito de esta funcion es pausar la
+    ejecucion hasta que el usuario precione una tecla
+    permitiendo leer los resultados antes de volver al menu
+    Parametros ninguno y no retorna nada.
+    """
+    system("pause")
+
+
+
+
+
+
